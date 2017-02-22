dc951fd2ec2a0809761f8e324da3164a259aa429
v1-115 <zoomtheclan@gmail.com>
2017-02-22 16:17:20 -0500

    Add files via upload

---
+++ code.py
@@ -0,0 +1,16 @@
+print       ("*instagram breaker*")
+print             ("made by V1.115" )
+
+
+import webbrowser
+
+webbrowser.open("https://www.instagram.com/?hl=en")
+
+print ("-d 1")
+
+import time 
+while True:
+    print ("")
+    time.sleep(2)
+
+
